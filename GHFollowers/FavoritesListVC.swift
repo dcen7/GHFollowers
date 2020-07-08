88a60ff4f91e39e22a2e8d6bcc33e82504b17aee
Deniz <mdcengiz@gmail.com>
2020-07-08 08:20:54 -0500

    first commit

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by Mehmet Deniz Cengiz on 7/7/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+    
+
+
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,20 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Mehmet Deniz Cengiz on 7/7/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+
+    
+}
