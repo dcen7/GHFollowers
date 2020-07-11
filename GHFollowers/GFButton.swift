6bfdc8cc8f3491bf7238691bca8b67499aecca50
Deniz <mdcengiz@gmail.com>
2020-07-11 08:32:48 -0500

    add image, text field button

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,41 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Mehmet Deniz Cengiz on 7/8/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    
+}
+
+
+
+
