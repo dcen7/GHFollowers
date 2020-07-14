19443d9b2407d72c21098f7e198af6575a577e3a
Deniz <mdcengiz@gmail.com>
2020-07-14 06:47:30 -0500

    Create modal

---
+++ GHFollowers/Modal/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  GHFollowers
+//
+//  Created by Mehmet Deniz Cengiz on 7/13/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatar: String
+}
