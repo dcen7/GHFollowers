948f2e2451a025ac732055ac7a15929dcd90dc9d
Deniz <mdcengiz@gmail.com>
2020-08-31 10:49:36 -0500

    Add ChildVC-UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Mehmet Deniz Cengiz on 8/30/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
