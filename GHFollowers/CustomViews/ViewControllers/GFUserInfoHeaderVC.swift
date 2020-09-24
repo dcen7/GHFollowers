4a7bd079ae219da68398ba5fbc80604b97c9bf54
Deniz <mdcengiz@gmail.com>
2020-09-24 16:04:56 -0500

    Convenience init, DateDecoding, Networking Recaftor

--- GHFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
+++ GHFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
@@ -37,7 +37,7 @@ class GFUserInfoHeaderVC: UIViewController {
     }
     
     func configureUIElement() {
-        avatarImageView.downloadImage(from: user.avatarUrl)
+        downloadAvatarImage()
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? "No Name"
         locationLabel.text = user.location ?? "No Location"
@@ -47,6 +47,15 @@ class GFUserInfoHeaderVC: UIViewController {
         locationImageView.image = UIImage(systemName: SFSymbols.location)
         locationImageView.tintColor = .secondaryLabel
         
+    }
+    
+    func downloadAvatarImage() {
+        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
+            guard let self =  self else { return }
+            DispatchQueue.main.async {
+                self.avatarImageView.image = image
+            }
+        }
     }
     
     func addSubviews() {
--- GHFollowers/CustomViews/Views/GFAlertContainerView.swift
+++ GHFollowers/CustomViews/Views/GFAlertContainerView.swift
@@ -1,33 +1,29 @@
 //
-//  GFAvatarImageView.swift
+//  GFAlertContainerView.swift
 //  GHFollowers
 //
-//  Created by Mehmet Deniz Cengiz on 7/18/20.
+//  Created by Mehmet Deniz Cengiz on 9/23/20.
 //  Copyright © 2020 Deniz Cengiz. All rights reserved.
 //
 
 import UIKit
 
-class GFAvatarImageView: UIImageView {
+class GFAlertContainerView: UIView {
 
-    let cache = NetworkManager.shared.cache
-    let placeholderImage = UIImage(named: "avatar-placeholder")!
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
-    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     private func configure() {
-        layer.cornerRadius = 10
-        clipsToBounds = true
-        image = placeholderImage
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
         translatesAutoresizingMaskIntoConstraints = false
     }
 
